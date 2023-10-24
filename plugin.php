142b885535f3c14ca9de743095cf21dff532c9af
Arseniy Olevskiy <arsidian@gmail.com>
2023-10-24 16:14:14 +0400

    regular commit

--- plugin.php
+++ plugin.php
@@ -10,6 +10,8 @@ License: GPLv2 or later
 Text Domain: ci_roman_numeral_date_converter
 */
 
+if (!defined('ABSPATH')) exit;
+
 if (!function_exists('add_shortcode')) return "No direct call for Roman Numeral Date Converter by Calculator.iO";
 
 function display_ci_roman_numeral_date_converter(){
